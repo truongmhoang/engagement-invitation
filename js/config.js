b5d8623bffa204472a099d668cfa7379a2a43660
Truong Hoang <moidoipass@gmail.com>
2026-08-22 07:15:59 -0400

    Update wedding invitation

--- js/config.js
+++ js/config.js
@@ -104,7 +104,7 @@ window.WEDDING_CONFIG = {
      ========================================================= */
   // Dán URL Apps Script Web App kết thúc bằng /exec vào đây.
   // Ví dụ: https://script.google.com/macros/s/AKfycb.../exec
-  googleSheetsEndpoint: "https://script.google.com/macros/s/AKfycbx_K2c_OPfdLy5mNhhuK9GvF88OLS0WRkreikBHk0ASASbSKl8JrhYHJeYl_oRoqvqSuQ/exec",
+  googleSheetsEndpoint: "https://script.google.com/macros/library/d/1XqZW1hH8i4LhPRf34DwgzzE6qwLz5zZE95M56-IJbr6hg9QQBaJfnCMP/2",
   googleSheetsTabName: "RSVP",
   rsvpEndpoint: ""
 };
